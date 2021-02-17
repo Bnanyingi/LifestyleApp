5f07296ad31caa0f8cbf70f6f2f0ab8cf59d817c
Alphonce <alphimwachoky@gmail.com>
2021-02-17 13:52:25 +0300

    more changes and i have added the log in/sign in page

--- app/src/main/java/com/example/lifestyleapplication/DashBoard.java
+++ app/src/main/java/com/example/lifestyleapplication/DashBoard.java
@@ -3,12 +3,15 @@ package com.example.lifestyleapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class DashBoard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //make activity full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dash_board);
     }
 }
