099bc85dcdbf33ef6318bd7515d5c0cd134df338
Lumbert Omwaga <61317911+LumbertEdward@users.noreply.github.com>
2021-05-11 17:50:02 +0300

    profile page

--- app/src/main/java/com/example/lifestyleapplication/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/lifestyleapplication/ui/auth/LoginFragment.kt
@@ -30,8 +30,7 @@ class LoginFragment : Fragment() {
 
         //Login
         binding.loginBtn.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_homeFragment2)
-            //authenticateUser()
+            authenticateUser()
         }
 
         //navigate to signup
